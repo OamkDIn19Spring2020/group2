777c23022f380ca9f56d5adec1333aded73ee90b
lasse <t8hula00@students.oamk.fi>
2020-03-24 18:28:39 +0200

    made a more sensible error messge

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -12,7 +12,7 @@ class home extends CI_Controller {
       if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
   {
           // Whoops, we don't have a page for that!
-          show_404();
+          echo 'whoops, file doesnt exist'
   }
 
     $data['title'] = ucfirst($page); // Capitalize the first letter
