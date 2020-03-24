a98a380e04ee004bc4d22857f6b74c2070e4a7c4
lasse <t8hula00@students.oamk.fi>
2020-03-24 18:21:10 +0200

    created homepage

---
+++ application/controllers/home.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class home extends CI_Controller {
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  function content($page = 'sales')
+  {
+      if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+  {
+          // Whoops, we don't have a page for that!
+          show_404();
+  }
+
+    $data['title'] = ucfirst($page); // Capitalize the first letter
+
+    $this->load->view('templates/header', $data);
+    $this->load->view('pages/'.$page, $data);
+    $this->load->view('templates/footer', $data);
+  }
+
+}
