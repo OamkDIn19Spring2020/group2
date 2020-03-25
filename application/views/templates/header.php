fd5fc695274a18200bc0a4f6e27c2a4bf263f423
lasse <t8hula00@students.oamk.fi>
2020-03-25 19:10:20 +0200

    trying out links

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,13 +1,13 @@
 <div id="header">
     <div id="tags">
-        <div class="buttons">sales</div>
+        <div class="buttons"><a href="<?php echo site_url('pages/sales'); ?>">Sales</a>sales</div>
         <div class="buttons">Categories</div>
-        <div class="buttons">Hot</div>
+        <div class="buttons"><a href="<?php echo site_url('pages/aboot'); ?>">Hot</a></div>
         <div class="buttons">Popular</div>
         <div class="buttons">New releases</div>
     </div>
     <div id='funcs'>
         <div id="Login">Login</div>
-        <div id="cart">Shopping Cart</div>        
+        <div id="cart">Shopping Cart</div>
     </div>
 </div>
