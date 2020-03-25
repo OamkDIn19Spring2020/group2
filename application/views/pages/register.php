fd8a9b26aa4bb2e55cbe8b8844518f1a77a7d868
lauri-mikkonen-oamkstudent <t9mila00@students.oamk.fi>
2020-03-25 17:09:56 +0200

    Making the registration form.

--- application/views/pages/register.php
+++ application/views/pages/register.php
@@ -9,9 +9,14 @@
   <div id="regContainer">
     <div id="regForm">
       <form action="register" method="post">
+        <label for="uname">Username:</label><br>
+        <input type="text" id="uname" name="uname">
         <fieldset class="fieldset">
           <legend>Password</legend>
-          
+          <label for="password">Password:</label><br>
+          <input type="text" id="pword" name="pword">
+          <label for="password">Repeat password:</label><br>
+          <input type="text" id="pword" name="pword">
         </fieldset>
       </form>
     </div>
