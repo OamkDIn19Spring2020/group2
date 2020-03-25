3ffba6a3c576facc5d18b094c2be3fc59d9609d6
lasse <t8hula00@students.oamk.fi>
2020-03-25 10:58:28 +0200

    still working of loading footer

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -16,7 +16,10 @@ class Home extends CI_Controller {
   }
 
     $data['page'] = $page;
-    $this->load->view('templates/content', $data);
+    $this->load->view('templates/header');
+    $this->load->view('pages/'.$data);
+    $this->load->view('templates/footer');
+    
   }
 
 }
