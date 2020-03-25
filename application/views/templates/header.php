34a28748aaae6d0121ff47c96b45de4e91648e92
lasse <t8hula00@students.oamk.fi>
2020-03-25 17:28:55 +0200

    headers styles

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,6 +6,8 @@
         <div class="buttons">Popular</div>
         <div class="buttons">New releases</div>
     </div>
-    <div id="Login">Login</div>
-    <div id="cart">Shopping Cart</div>
+    <div id='funcs'>
+        <div id="Login">Login</div>
+        <div id="cart">Shopping Cart</div>        
+    </div>
 </div>
