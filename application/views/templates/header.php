fbbaabe1ff185a8cae10e3b2517a01e53a20a4de
lasse <t8hula00@students.oamk.fi>
2020-03-25 10:52:59 +0200

    hopefully footer is visible now

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,7 @@
+<!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title>Home</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
