cf5a2af019bbe734a664940e6495bb586367474b
lasse <t8hula00@students.oamk.fi>
2020-03-25 11:48:00 +0200

    fixed missing . in line 4

--- application/views/templates/body.php
+++ application/views/templates/body.php
@@ -1,7 +1,7 @@
 <body>
     <div>
         <?php $this->load->view('templates/header'); ?>
-        <?php $this->load->view('pages/'$page); ?>
+        <?php $this->load->view('pages/'.$page); ?>
         <?php $this->load->view('templates/footer'); ?>
     </div>
 </body>
