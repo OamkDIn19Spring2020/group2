65039155348a870631df321568368288d42f945d
lasse <t8hula00@students.oamk.fi>
2020-03-26 11:38:45 +0200

    testing add user

---
+++ application/controllers/Aboot.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Aboot extends CI_Controller {
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('user');
+  }
+
+  function getusers(){
+      $this->load->helper('url');
+        $data['user'] = $this->Aboot->getusers();
+        $data['page'] = $page;
+        $this->load->view('templates/page', $data);
+  }
+
+  public function adduser(){
+    //print_r($this->input->post());
+    $plainpass = $this->input->post('password');
+    $hash = password_hash($plainpass, PASSWORD_DEFAULT);
+
+    $insert_data=array(
+      'username'=>$this->input->post('username'),
+      'email'=>$this->input->post('email'),
+      'password'=>$hash,
+      'phone'=>$this->input->post('phone'),
+      'country'=>$this->input->post('country'),
+    );
+    $test=$this->Aboot->adduser($insert_data);
+    //echo 'inserted '.$test. 'books';
+    redirect('aboot');
+
+}
