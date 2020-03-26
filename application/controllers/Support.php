39003dd7180b5b136285683611504700ea33b284
Nabeel <nabeelbaig962@gmail.com>
2020-03-26 08:39:11 +0200

    added customer support link and page to the main page. added img folder and one image.

---
+++ application/controllers/Support.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Support extends CI_Controller{
+
+  function content($page = 'support')
+  {
+    if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+    {
+            // Whoops, we don't have a page for that!
+            echo 'whoops, file doesnt exist';
+    }
+  }
+  $data['page'] = $page;
+  $this->load->view('templates/page', $data);
+}
