79d50ebba455208662f585d41187e7ae0a95f29d
Nabeel <nabeelbaig962@gmail.com>
2020-03-29 16:18:27 +0300

    tweaking text fields

--- application/views/pages/support.php
+++ application/views/pages/support.php
@@ -4,6 +4,7 @@
 You can leave us a message in the field below or call our <br>
 Customer Support Toll-Free number: +91 45 6584 1937
 </div>
+<input type="text">
 <input type="text" style="height:170px; width:400px;">
 <input type="submit">
 <img id="support_pic" src="../assests/imgs/berkeley-communications-WEDDt-u3q3o-unsplash.jpg">
