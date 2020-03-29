0981575a54b45bcaf7d7d1f13bd0a6f9b4fd82f1
lasse <t8hula00@students.oamk.fi>
2020-03-29 15:25:44 +0300

    creating search for games

--- application/models/Games.php
+++ application/models/Games.php
@@ -13,4 +13,11 @@ class Games extends CI_Model {
       $this->db->from('games');
       return $this->db->get()->result_array();
   }
+  public function sortcat($id_game = '%')
+  {
+     $this->db->select('*');
+     $this->db->from('games');
+     $this->db->where('idGame Like "'.$id_game.'"');
+     return $this->db->get()->result_array();
+  }
 }
