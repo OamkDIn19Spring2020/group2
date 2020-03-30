7252fe3bef3630296eb6c3129c8f86525c9244f6
Nabeel <nabeelbaig962@gmail.com>
2020-03-30 11:52:11 +0300

    modifying support page for styling.

--- application/views/pages/support.php
+++ application/views/pages/support.php
@@ -4,6 +4,9 @@
 You can leave us a message in the field below or call our <br>
 Customer Support Toll-Free number: +91 45 6584 1937
 </div>
+<div id ='support_text'>
+<input type="text">
 <input type="text" style="height:170px; width:400px;">
 <input type="submit">
+</div>
 <img id="support_pic" src="../assests/imgs/berkeley-communications-WEDDt-u3q3o-unsplash.jpg">
