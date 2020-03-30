57531737a43f56b026d1ec41e2fbe91fcb046743
lasse <t8hula00@students.oamk.fi>
2020-03-30 11:17:18 +0300

    fixed category sort error

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,6 +8,7 @@ class Home extends CI_Controller {
     $this->load->model('Games_model');
     $this->load->model('Aboot_model');
     $this->load->model('Users_model');
+    $this->load->model('Category_model');
   }
 
   function content($page = 'sales')
@@ -19,6 +20,7 @@ class Home extends CI_Controller {
   }
     $data['games'] = $this->Games_model->getgames();
     $data['user'] = $this->Aboot_model->getusers();
+    $data['cats'] = $this->Category_model->sortcat();
     $data['page'] = $page;
     $this->load->view('templates/page', $data);
 
