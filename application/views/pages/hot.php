492acfba9ebf567f3ca5a54756836398dc23ddcf
lasse <t8hula00@students.oamk.fi>
2020-04-01 12:23:33 +0300

    final changes to hot

--- application/views/pages/hot.php
+++ application/views/pages/hot.php
@@ -23,7 +23,7 @@
         <?php
         foreach ($hot as $row){
             echo '<tr>';
-            echo '<td>'.$row['idGame'].'</td><td>'.$row['price'].'</td><td>'.$row['p_count'].'%</td><td>'.$row['developer'].'</td><td>'.$row['publisher'].'</td><td>'.$row['r_date'].'</td><td>'.$row['category'].'</td><td>';
+            echo '<td>'.$row['idGame'].'</td><td>'.$row['price'].'</td><td>'.$row['p_count'].'</td><td>'.$row['developer'].'</td><td>'.$row['publisher'].'</td><td>'.$row['r_date'].'</td><td>'.$row['category'].'</td><td>';
             echo '</tr>';
         }
          ?>
