4c6b0a7682b7c85d24207918d1d19b8d10334f39
Nabeel <nabeelbaig962@gmail.com>
2020-04-01 12:26:36 +0300

    Added a droplist for issues, changed the picture to something more approriate to the theme, added placeholder message to the message box, picture moved to the right, and submit button plays a sound.

--- application/views/pages/support.php
+++ application/views/pages/support.php
@@ -1,12 +1,27 @@
+<script>
+function playSound () {
+    document.getElementById('play').play();
+}
+</script>
 <div>
 <h1>Customer Support</h1>
 <p>Having Problems with your purchase? No Problem! <br>
 You can leave us a message in the field below or call our <br>
 Customer Support Toll-Free number: +91 45 6584 1937
 </div>
-<div id ='support_text'>
-<input type="text">
-<input type="text" style="height:170px; width:400px;">
-<input type="submit">
+<p>Please select your issue below:</p>
+<select>
+<option>Invalid Key, You are scam!</option>
+<option>Problem with installation</option>
+<option>Trojan horse detected :(</option>
+<option>Chunguscoin is not showing value.</option>
+
+</select>
+<div>
+<input type="text" placeholder="Write your message here..." style="height:170px; width:400px;">
+<audio id="play" src="../assests/sounds/Big chungus - original clip.mp3"></audio>
+<input type="submit" onclick="playSound()">
 </div>
-<img id="support_pic" src="../assests/imgs/berkeley-communications-WEDDt-u3q3o-unsplash.jpg">
+<div id="pic_border">
+<img id="support_pic" src="../assests/imgs/2_ohMC30wCR_gMzmxiUxnDCQ.jpeg">
+</div>
