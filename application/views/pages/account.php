6aeed331e2595bd799ce8969c5b43350d67b5c2d
lasse <t8hula00@students.oamk.fi>
2020-04-02 18:24:34 +0300

    changed destroy_session -> session_destroy

--- application/views/pages/account.php
+++ application/views/pages/account.php
@@ -1,3 +1,3 @@
 <?php echo $_SESSION['username'] ?>
 <br>
-<button type="button" onclick="<?php destroy_session(); redirect('sales');?>">logout</button>
+<button type="button" onclick="<?php session_destroy(); redirect('sales');?>">logout</button>
