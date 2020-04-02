88fde573e79e5c8579cee819796cb7f2e08d64c0
lasse <t8hula00@students.oamk.fi>
2020-04-02 11:17:59 +0300

    starting proper css changes

--- application/views/templates/body.php
+++ application/views/templates/body.php
@@ -1,7 +1,9 @@
 <body>
         <?php $this->load->view('templates/header'); ?>
         <div id="contents">
+            <div id="center">
             <?php $this->load->view('pages/'.$page); ?>
+            </div>
         </div>
         <?php  $this->load->view('templates/foot'); ?>
 </body>
