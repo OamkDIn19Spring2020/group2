b2f0e0e787231e3cd9075e3aae7cad8278bbf1e8
lasse <t8hula00@students.oamk.fi>
2020-04-02 11:42:22 +0300

    footer css changes

--- application/views/templates/foot.php
+++ application/views/templates/foot.php
@@ -1,4 +1,18 @@
 <div id="footer">
-    <h3>nabeels house</h3>
+    <div id="footext">
+        CHUNGUSGAINZ
+    </div>
+    <div id="footercont">
+        <div class = 'buttons'><a href="<?php echo site_url('aboot'); ?>">aboot.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('cart'); ?>">cart.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('category'); ?>">category.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('games'); ?>">games.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('hot'); ?>">hot.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('register'); ?>">register.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('regsuccess'); ?>">regsuccess.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('sales'); ?>">sales.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('support'); ?>">support.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('users'); ?>">users.php</a></div>
+    </div>
     <div class = 'buttons'><a href="<?php echo site_url('support'); ?>">Customer Support</a></div>
 </div>
