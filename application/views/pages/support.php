a8553362f5ac9d157936b6495d29c0a479e371b0
Nabeel <nabeelbaig962@gmail.com>
2020-04-03 15:54:51 +0300

    modified support.php and styles.css for fine tuning picture.

--- application/views/pages/support.php
+++ application/views/pages/support.php
@@ -9,6 +9,9 @@ function playSound () {
 You can leave us a message in the field below or call our <br>
 Customer Support Toll-Free number: +91 45 6584 1937
 </div>
+
+<div id = "message_box">
+<div>
 <p>Please select your issue below:</p>
 <select>
 <option>Invalid Key, You are scam!</option>
@@ -16,11 +19,11 @@ Customer Support Toll-Free number: +91 45 6584 1937
 <option>Trojan horse detected :(</option>
 <option>Chunguscoin is not showing value.</option>
 </select>
-<div>
 <input type="text" placeholder="Write your message here..." style="height:170px; width:400px;">
 <audio id="play" src="../assests/sounds/Big chungus - original clip.mp3"></audio>
 <input type="submit" onclick="playSound()">
 </div>
 <div id="pic_border">
 <img id="support_pic" src="../assests/imgs/2_ohMC30wCR_gMzmxiUxnDCQ.jpeg">
+</div>
 </div>
