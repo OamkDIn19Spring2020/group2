7ec98bbc698697ab3b4eee91635a3c0db88c3360
lasse <t8hula00@students.oamk.fi>
2020-04-03 16:50:05 +0300

    support page css changes

--- application/views/pages/support.php
+++ application/views/pages/support.php
@@ -13,9 +13,6 @@ function playSound () {
 You can leave us a message in the field below or call our <br>
 Customer Support Toll-Free number: +91 45 6584 1937
 </div>
-<div id="pic_border">
-    <img id="support_pic" src="../assests/imgs/2_ohMC30wCR_gMzmxiUxnDCQ.jpeg">
-</div>
 </div>
 <div id="suppfunc">
 
@@ -31,6 +28,9 @@ Customer Support Toll-Free number: +91 45 6584 1937
 <input type="text" placeholder="Write your message here..." style="height:170px; width:400px;">
 <audio id="play" src="../assests/sounds/Big chungus - original clip.mp3"></audio>
 <input type="submit" onclick="playSound()">
+</div>
+<div id="pic_border">
+    <img id="support_pic" src="../assests/imgs/2_ohMC30wCR_gMzmxiUxnDCQ.jpeg">
 </div>
 </div>
 </div>
