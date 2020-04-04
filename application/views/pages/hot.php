0afc93b1e9e9bd5be5d4273063dab9b66c253659
lasse <t8hula00@students.oamk.fi>
2020-04-04 15:30:56 +0300

    trying table changes

--- application/views/pages/hot.php
+++ application/views/pages/hot.php
@@ -13,14 +13,47 @@
 
  </fieldset>
 </form>
-    <table>
+
+<form action="Show_c/index" method="post">
+
+<div class="table">
+    <div class="tr">
+        <div class="td">Game</div>
+        <div class="td">price</div>
+        <div class="td">Amount Purchased</div>
+        <div class="td">developer</div>
+        <div class="td">publisher</div>
+        <div class="td">release date</div>
+        <div class="td">categories</div>
+    </div>
+    <?php
+    foreach ($hot as $row) {
+        echo '<button type="submit"><button name="gname" value='.$row['idGame']'><div class="tr">
+            <div class="td">'.$row['idGame'].'</div>
+            <div class="td">'.$row['price'].'</div>
+            <div class="td">'.$row['p_count'].'</div>
+            <div class="td">'.$row['developer'].'</div>
+            <div class="td">'.$row['publisher'].'</div>
+            <div class="td">'.$row['r_date'].'</div>
+            <div class="td">'.$row['category'].'</div>
+            </div></button></button>';
+    }
+
+     ?>
+</div>
+
+</form>
+
+
+
+<!--    <table>
         <thead>
             <tr>
                 <th>Game</th><th>price</th><th>Amount purchased</th><th>developer</th><th>publisher</th><th>release date</th><th>categories</th>
             </tr>
         </thead>
         <tbody>
-            <?php
+
             foreach ($hot as $row){
                 echo '<tr>';
                 echo '<td><form action='.site_url('Show_c/index').' method="post"><button type="submit" value='.$row['idGame'].' name="gname">'.$row['idGame'].'</button></form></td>
@@ -32,6 +65,7 @@
                 <td><form action='.site_url('Show_c/index').' method="post"><button type="submit" value='.$row['idGame'].' name="gname">'.$row['category'].'</button></form></td>';
                 echo '</tr>';
             }
-            ?>
+
         </tbody>
     </table>
+-->
