3e902c89bc1a6f825d628de3e04900c6df10a663
lasse <t8hula00@students.oamk.fi>
2020-04-05 13:39:21 +0300

    texft -> textf

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -1,5 +1,5 @@
 <?php
-$file = fopen(base_url('assests/texft/'.$gname.'.txt'), 'r') or die('file not found');
+$file = fopen(base_url('assests/textf/'.$gname.'.txt'), 'r') or die('file not found');
 echo fread($file, filesize($file));
 fclose($file);
 ?>
