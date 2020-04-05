8a27c93cf2eaacd65cd57350fdd461e167af7fc2
lasse <t8hula00@students.oamk.fi>
2020-04-05 11:52:24 +0300

    things

--- application/controllers/Show_c.php
+++ application/controllers/Show_c.php
@@ -9,7 +9,7 @@ class Show_c extends CI_Controller {
 
   function index()
   {
-      $parts = parse_url('193.167.100.74/~t8hula00/group2/index.php/Show_c/index/$1');
+      $parts = parse_url(site_url());
       $pathparts = explode('/', $parts['path']);
       print_r($pathparts);
       $gname = parse_url('193.167.100.74/~t8hula00/group2/index.php/Show_c/index/$1');
