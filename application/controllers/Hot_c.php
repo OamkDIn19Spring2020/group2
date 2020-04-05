fdff7becafd6f882e9324513bfb9dc88460220e5
lasse <t8hula00@students.oamk.fi>
2020-04-05 19:24:09 +0300

    trying out spaghetti code

--- application/controllers/Hot_c.php
+++ application/controllers/Hot_c.php
@@ -29,7 +29,6 @@ class Hot_c extends CI_Controller {
 
     function hotgames(){
         $period = $this->input->post('interval');
-        $_SESSION['inter'] = $period;
         $data['hot'] = $this->Games_model->gethot($period);
         $data['page'] = 'hot';
         $data['inter'] = $period;
