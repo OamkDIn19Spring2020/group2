ac3046077883c4794a3f75c3926e81ba9db8fa3b
lasse <t8hula00@students.oamk.fi>
2020-04-05 11:48:17 +0300

    trying parse_url

--- application/controllers/Show_c.php
+++ application/controllers/Show_c.php
@@ -10,7 +10,7 @@ class Show_c extends CI_Controller {
   function index()
   {
       $parts = parse_url('193.167.100.74/~t8hula00/group2/index.php/Show_c/index/$1');
-      r_print($parts);
+      print_r($parts);
       $gname = parse_url('193.167.100.74/~t8hula00/group2/index.php/Show_c/index/$1');
       $data['page']  = 'aboot';
       $this->load->view('templates/page', $data);
