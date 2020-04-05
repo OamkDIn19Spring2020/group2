835041c76bc053ae398a661c2b6eb230b2dbd7cf
lasse <t8hula00@students.oamk.fi>
2020-04-05 13:40:39 +0300

    trying to get filesize to work

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -1,5 +1,5 @@
 <?php
 $file = fopen(base_url('assests/textf/'.$gname.'.txt'), 'r') or die('file not found');
-echo fread($file, filesize($file));
+echo fread($file, filesize(base_url('assests/textf/'.$gname.'.txt')));
 fclose($file);
 ?>
