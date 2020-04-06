5c2ea9ecd981c03a58313db159b2ab5aa4707209
lasse <t8hula00@students.oamk.fi>
2020-04-06 12:36:13 +0300

    things

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -2,6 +2,7 @@
 
 
 <?php
+echo $gname;
 $file = fopen(base_url('assests/textf/'.$gname.'.txt'), 'r') or die('file not found');
 echo fread($file, filesize('assests/textf/'.$gname.'.txt'));
 fclose($file);
