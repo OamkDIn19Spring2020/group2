1536cbf2e5d20a83fce4b31019e2cf00037321e2
lasse <t8hula00@students.oamk.fi>
2020-04-06 11:04:22 +0300

    trying grid layout changes

--- application/views/pages/games.php
+++ application/views/pages/games.php
@@ -89,11 +89,9 @@
                         <a class="gameimgTable" href='.site_url('Show_c/index/'.$row['idGame']).'>
                         <img class="gameimgTable" src='.base_url().'assests/imgs/'.$row['idGame'].'.jpg>
                         </a>
-                        <a class="gameNameTable" href='.site_url('Show_c/index/'.$row['idGame']).'><div class="text">'.$row['idGame'].'</div></a>
-                        <div class="priceTable">
-                            <a href='.site_url('Show_c/index/'.$row['idGame']).'>
+                        <a class="gameNameTable" href='.site_url('Show_c/index/'.$row['idGame']).'><div class="text">'.$row['idGame'].'
                                 <div class="gamePrice">'.$row['price'].'¤</div>
-                            </a>
+                                </div></a>
                             <div class="addbutton">
                                 <form action="Cart_c/addtocart" method="post">
                                     <button value='.$row['idGame'].' name="submit" type="submit">add to cart</button>
