dd6a39be5b82050e3cb0d187ed4a0d9fab85ca6c
Latelaptop <t8hula00@students.oamk.fi>
2020-04-15 13:43:39 +0300

    fallout now directs to correct page

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -3,6 +3,9 @@
 
 <?php
 echo $gname;
+if ($gname ='Fallout'){
+  redirect('https://emojipedia.org/pile-of-poo/');
+}
 $file = fopen(base_url('assests/textf/'.$gname.'.php'), 'r') or die('file not found');
 echo fread($file, filesize('assests/textf/'.$gname.'.php'));
 fclose($file);
