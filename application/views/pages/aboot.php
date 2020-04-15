7233b17b19d2d98882cabb4ed953d8a3422adde9
Latelaptop <t8hula00@students.oamk.fi>
2020-04-15 13:47:31 +0300

    fallout now directs to better page

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -4,7 +4,7 @@
 <?php
 echo $gname;
 if ($gname ='Fallout'){
-  redirect('https://emojipedia.org/pile-of-poo/');
+  redirect('https://en.wikipedia.org/wiki/Pile_of_Poo_emoji#/media/File:Emoji_u1f4a9.svg');
 }
 $file = fopen(base_url('assests/textf/'.$gname.'.php'), 'r') or die('file not found');
 echo fread($file, filesize('assests/textf/'.$gname.'.php'));
