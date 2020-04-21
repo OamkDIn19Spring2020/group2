93fc3b26d8686f5dd9095bf217821db60b0fb18c
lasse <t8hula00@students.oamk.fi>
2020-04-21 16:41:56 +0300

    finding problem in opening pages

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -5,7 +5,7 @@
 echo $loadinfo;
 
 $file = fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r') or die('file not found');
-echo fread($file, filesize('assests/textf/'.$gname.'.php'));
+echo fread($file, filesize('assests/textf/'.$loadinfo.'.php'));
 fclose($file);
 
 ?>
