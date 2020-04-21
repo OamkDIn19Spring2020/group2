7fabed05cf4f98097754166e9f442d19b07866d0
lasse <t8hula00@students.oamk.fi>
2020-04-21 16:52:11 +0300

    fixing problems on fallout s page

--- assests/textf/Fallout_76.php
+++ assests/textf/Fallout_76.php
@@ -1,4 +1,2 @@
-<?php
-echo 'aaaa'
-    redirect('https://en.wikipedia.org/wiki/Pile_of_Poo_emoji#/media/File:Emoji_u1f4a9.svg');
- ?>
+
+<div onload=window.location="https://en.wikipedia.org/wiki/Pile_of_Poo_emoji#/media/File:Emoji_u1f4a9.svg"/>
