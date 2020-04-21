21a04fa4e441835e071d03eda5238ae808a40105
lasse <t8hula00@students.oamk.fi>
2020-04-21 18:06:15 +0300

    made default page if no games found

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -2,8 +2,9 @@
 
 
 <?php
+$pageifnonefound = $loadinfo." is a game about things where you do stuff. <br>It was release at some point, near the release of another game. <br>Core features include nothing and everything.";
 
-$file = fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r') or die('file not found');
+$file = fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r') or echo $pageifnonefound;
 echo fread($file, filesize('assests/textf/'.$loadinfo.'.php'));
 fclose($file);
 
