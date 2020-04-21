fffed64da5da6679ed6de9c061e0abe54e71faa9
lasse <t8hula00@students.oamk.fi>
2020-04-21 16:49:19 +0300

    fixed problems in loading game pages

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -2,10 +2,7 @@
 
 
 <?php
-echo $loadinfo;
-if($loadinfo = "Fallout_76"){
-    redirect('https://en.wikipedia.org/wiki/Pile_of_Poo_emoji#/media/File:Emoji_u1f4a9.svg');
-}
+
 $file = fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r') or die('file not found');
 echo fread($file, filesize('assests/textf/'.$loadinfo.'.php'));
 fclose($file);
--- assests/textf/Fallout_76.php
+++ assests/textf/Fallout_76.php
@@ -1,3 +1,4 @@
 <?php
+echo 'aaaa'
     redirect('https://en.wikipedia.org/wiki/Pile_of_Poo_emoji#/media/File:Emoji_u1f4a9.svg');
  ?>
