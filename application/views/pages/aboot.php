0220f1c2f23b209c8435e58d0cb0a07f9f58a1d8
lasse <t8hula00@students.oamk.fi>
2020-04-21 18:10:13 +0300

    hopefully backuppage now loads properly

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -4,10 +4,14 @@
 <?php
 $pageifnonefound = $loadinfo." is a game about things where you do stuff. <br>It was release at some point, near the release of another game. <br>Core features include nothing and everything.";
 
-$file = fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r') or die($pageifnonefound);
-echo fread($file, filesize('assests/textf/'.$loadinfo.'.php'));
-fclose($file);
-
+if(fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r')){
+    $file = fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r');
+    echo fread($file, filesize('assests/textf/'.$loadinfo.'.php'));
+    fclose($file);
+}
+else{
+    echo $pageifnonefound;
+}
 ?>
 
 </div>
