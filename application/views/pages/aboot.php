45f3341fbc8541dde01a78630ded8b03436af2fd
lasse <t8hula00@students.oamk.fi>
2020-04-21 18:17:28 +0300

    game names now display properly in backup page

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -2,7 +2,7 @@
 
 
 <?php
-$pageifnonefound = $loadinfo." is a game about things where you do stuff. <br>It was release at some point, near the release of another game. <br>Core features include nothing and everything.";
+$pageifnonefound = $loadinfo." is a game about things where you do stuff. <br>It was released at some point, near the release of another game. <br>Core features include nothing and everything.";
 
 if(file_exists(base_url('assests/textf/'.$loadinfo.'.php'))){
     $file = fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r');
@@ -10,6 +10,13 @@ if(file_exists(base_url('assests/textf/'.$loadinfo.'.php'))){
     fclose($file);
 }
 else{
+    if(strpos($loadinfo, '_')){
+        $loadinfo = str_replace('_', ' ', $loadinfo);
+        if(strpos($loadinfo, '  ')){
+            $loadinfo = str_replace('  ', ': ', $loadinfo);
+        }
+    }
+
     echo $pageifnonefound;
 }
 ?>
