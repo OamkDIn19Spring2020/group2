bf301c1986c3e09e0d2a3099d6c5b903633a4048
lasse <t8hula00@students.oamk.fi>
2020-04-21 18:25:39 +0300

    doing weird shit with

--- application/views/pages/account.php
+++ application/views/pages/account.php
@@ -3,4 +3,4 @@
 <form  action="<?php echo site_url('Home/logout');?>" method="post">
     <input type="submit" value="Log out">
 </form>
-<?php print_r($SESSION['testarray']); ?>
+<?php print_r($_SESSION['testarray']); ?>
