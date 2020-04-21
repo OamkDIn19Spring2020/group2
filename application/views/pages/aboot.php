a6bb77eeab97c7cdf5a7c90acc6bbc7ccc29ba24
lasse <t8hula00@students.oamk.fi>
2020-04-21 16:33:31 +0300

    finding problemin opening pages

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -3,12 +3,12 @@
 
 <?php
 echo $gname;
-if ($gname ='Fallout_76'){
-  redirect('https://en.wikipedia.org/wiki/Pile_of_Poo_emoji#/media/File:Emoji_u1f4a9.svg');
-}
-$file = fopen(base_url('assests/textf/'.$gname.'.php'), 'r') or die('file not found');
-echo fread($file, filesize('assests/textf/'.$gname.'.php'));
-fclose($file);
+//if ($gname ='Fallout_76'){
+  //redirect('https://en.wikipedia.org/wiki/Pile_of_Poo_emoji#/media/File:Emoji_u1f4a9.svg');
+//}
+//$file = fopen(base_url('assests/textf/'.$gname.'.php'), 'r') or die('file not found');
+//echo fread($file, filesize('assests/textf/'.$gname.'.php'));
+//fclose($file);
 ?>
 
 </div>
