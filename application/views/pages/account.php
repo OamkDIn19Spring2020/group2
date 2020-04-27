83aa751d0aed0c3afeddf367505988b283ac8db3
lasse <t8hula00@students.oamk.fi>
2020-04-27 14:47:04 +0300

    finxing bugs

--- application/views/pages/account.php
+++ application/views/pages/account.php
@@ -1,9 +1,11 @@
 <?php echo $_SESSION['username'] ?>
 <br>
 <?php
-foreach ($userinfo as $row) {
-    echo $row;
-}
+echo $userinfo['username'].'<br>';
+echo $userinfo['email'].'<br>';
+echo $userinfo['phone'].'<br>';
+echo $userinfo['country'].'<br>';
+echo $userinfo['password'].'<br>';
 ?>
 <form  action="<?php echo site_url('Home/logout');?>" method="post">
     <input type="submit" value="Log out">
