f4a756fdb8fd0c84b91852419c24190a5e220578
lasse <t8hula00@students.oamk.fi>
2020-04-27 13:34:27 +0300

    things

--- application/views/pages/cart.php
+++ application/views/pages/cart.php
@@ -1,44 +1,4 @@
 <div id="cartcont">
-
-<form action=" <?php echo site_url('Cart_c/index') ?> " method="post">
-  <fieldset class="fieldset">
-    <label for="uname">user</label><br>
-    <select class="uname" name="uname">
-        <?php
-            foreach ($user as $key) {
-                echo '<option value="'.$key['username'].'">'.$key['username'].'</option>';
-            }
-        ?>
-    </select><br>
-
-    <label for="price">Price</label><br>
-    <input type="number" id="price" name="price"><br>
-
-    <label for="method">method</label<br>
-    <select class="meth" name="meth">
-        <option value="skrill">Skrill</option>
-        <option value="chunguscoin">Chunguscoin</option>
-        <option value="paypal">Paypal</option>
-        <option value="renminbi">Renminbi</option>
-    </select><br>
-
-    <label for="promo">Promocode</label><br>
-    <input type="promocode" name="promo"><br>
-
-    <label for="game">game</label><br>
-    <select class="game" name="game">
-        <?php
-        foreach ($games as $row) {
-            echo '<option value="'.$row['idGame'].'">'.$row['idGame'].'</option>';
-        }
-         ?>
-    </select>
-
-
-    <input type="submit" value="submit">
-
- </fieldset>
-</form>
 <h1>Here are your items:</h1>
 
         <?php
@@ -66,4 +26,20 @@
             }
         }
          ?>
+         <form action=" <?php echo site_url('Cart_c/index') ?> " method="post">
+           <fieldset class="fieldset">
+             <label for="method">method</label<br>
+             <select class="meth" name="meth">
+                 <option value="skrill">Skrill</option>
+                 <option value="chunguscoin">Chunguscoin</option>
+                 <option value="paypal">Paypal</option>
+                 <option value="renminbi">Renminbi</option>
+             </select><br>
+
+             <label for="promo">Promocode</label><br>
+             <input type="promocode" name="promo"><br>
+             <input type="submit" value="submit">
+
+          </fieldset>
+         </form>
 </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -11,7 +11,7 @@
         <?php
         if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
             echo '<a href='.site_url('account').' id="Login">'.$_SESSION['username'].'</a>';
-            echo '<a href='.site_url('cart').' id="cart">Shopping cart     <span color="red;">'.count($_SESSION['testarray']).'</span></a>';
+            echo '<a href='.site_url('cart').' id="cart">Shopping cart     <span style="color:red;">'.count($_SESSION['testarray']).'</span></a>';
         }
         else{
             echo "<div id='logreg'>";
