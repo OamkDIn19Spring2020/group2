78291afb60531aff460e55a641c33f805e1d304a
Nabeel <nabeelbaig962@gmail.com>
2020-04-28 14:42:23 +0300

    Merged Lauri's account delete button and removed some links from the footer.

--- application/views/pages/account.php
+++ application/views/pages/account.php
@@ -9,15 +9,6 @@
     // code...
     }
 
-<<<<<<< HEAD
-    ?>
-    <button type="button" name="button" ><a href="<?php echo site_url('Cart_c/history')?>">Purchase History</a></button>
-    <button type="button" name="button" ><a href="<?php echo site_url('changepass')?>">change password</a></button>
-    <form  action="<?php echo site_url('Home/logout');?>" method="post">
-        <input type="submit" value="Log out">
-    </form>
-</div>
-=======
 ?>
 <button type="button" name="button" ><a href="<?php echo site_url('Cart_c/history')?>">Purchase History</a></button>
 <button type="button" name="button" ><a href="<?php echo site_url('changepass')?>">change password</a></button>
@@ -25,4 +16,3 @@
 <form  action="<?php echo site_url('Home/logout');?>" method="post">
     <input type="submit" value="Log out">
 </form>
->>>>>>> master
--- application/views/templates/foot.php
+++ application/views/templates/foot.php
@@ -3,16 +3,12 @@
         <h3>CHUNGUSGAINZ.COM.NET.CN.RU</h3>
     </div>
     <div id="footercont">
-        <div class = 'buttons'><a href="<?php echo site_url('aboot'); ?>">aboot.php</a></div>
-        <div class = 'buttons'><a href="<?php echo site_url('cart'); ?>">cart.php</a></div>
-        <div class = 'buttons'><a href="<?php echo site_url('category'); ?>">category.php</a></div>
-        <div class = 'buttons'><a href="<?php echo site_url('games'); ?>">games.php</a></div>
-        <div class = 'buttons'><a href="<?php echo site_url('hot'); ?>">hot.php</a></div>
-        <div class = 'buttons'><a href="<?php echo site_url('register'); ?>">register.php</a></div>
-        <div class = 'buttons'><a href="<?php echo site_url('regsuccess'); ?>">regsuccess.php</a></div>
-        <div class = 'buttons'><a href="<?php echo site_url('sales'); ?>">sales.php</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('category'); ?>">Category</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('games'); ?>">Games</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('hot'); ?>">Hot</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('register'); ?>">Register</a></div>
+        <div class = 'buttons'><a href="<?php echo site_url('sales'); ?>">Sales</a></div>
         <div class = 'buttons' <?php if(!isset($_SESSION['username'])){echo 'style="display:none;"';}?>><a href="<?php echo site_url('support'); ?>">support.php</a></div>
-        <div class = 'buttons'><a href="<?php echo site_url('users'); ?>">users.php</a></div>
     </div>
     <div class = 'buttons' <?php if(!isset($_SESSION['username'])){echo 'style="display:none;"';}?>><a href="<?php echo site_url('support'); ?>">Customer Support</a></div>
 </div>
