c75ad8ba2abb719a039e843168b662d1f9083b7b
lasse <t8hula00@students.oamk.fi>
2020-04-28 13:49:48 +0300

    fixing things

--- application/controllers/Cart_c.php
+++ application/controllers/Cart_c.php
@@ -58,8 +58,7 @@ class Cart_c extends CI_Controller {
     else{
     $data['codetext'] = '<p>Code Invalid!</p>';
   }
-  $data['page'] = 'cart';
-  $this->load->view('templates/page', $data);
+  redirec('cart', $data);
   }
 
 }
