7819388fef331e9bf1b39d5aae239dade042fd35
Nabeel <nabeelbaig962@gmail.com>
2020-04-28 16:58:39 +0300

    removed extra support link.

--- application/views/templates/foot.php
+++ application/views/templates/foot.php
@@ -6,7 +6,6 @@
         <div class = 'buttons'><a href="<?php echo site_url('hot'); ?>">Hot</a></div>
         <div class = 'buttons'><a href="<?php echo site_url('register'); ?>">Register</a></div>
         <div class = 'buttons'><a href="<?php echo site_url('sales'); ?>">Sales</a></div>
-        <div class = 'buttons' <?php if(!isset($_SESSION['username'])){echo 'style="display:none;"';}?>><a href="<?php echo site_url('support'); ?>">support.php</a></div>
     </div>
     <div class = 'buttons' <?php if(!isset($_SESSION['username'])){echo 'style="display:none;"';}?>><a href="<?php echo site_url('support'); ?>">Customer Support</a></div>
 </div>
