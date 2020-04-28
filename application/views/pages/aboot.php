1714a4e4d1940390118cc05c2a7914d72410f819
lasse <t8hula00@students.oamk.fi>
2020-04-28 15:15:28 +0300

    fixing loading game info pages

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -3,22 +3,22 @@
 
 <?php
 
-if(fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r')){
+if(!fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r')){
+    if(strpos($loadinfo, '_')){
+        $loadinfo = str_replace('_', ' ', $loadinfo);
+        if(strpos($loadinfo, '  ')){
+            $loadinfo = str_replace('  ', ': ', $loadinfo);
+        }
+    }
+    $pageifnonefound = $loadinfo." is a game about things where you do stuff. <br>It was released at some point, near the release of another game. <br>Core features include nothing and everything.";
+    echo $pageifnonefound;
+}
+else{
     $file = fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r');
     echo fread($file, filesize('assests/textf/'.$loadinfo.'.php'));
     fclose($file);
 }
-else{
 
-if(strpos($loadinfo, '_')){
-$loadinfo = str_replace('_', ' ', $loadinfo);
-if(strpos($loadinfo, '  ')){
-$loadinfo = str_replace('  ', ': ', $loadinfo);
-}
-}
-    $pageifnonefound = $loadinfo." is a game about things where you do stuff. <br>It was released at some point, near the release of another game. <br>Core features include nothing and everything.";
-    echo $pageifnonefound;
-}
 ?>
 
 </div>
