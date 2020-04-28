0b31bbb870474f339ee152c2834034e8f98ba33c
lasse <t8hula00@students.oamk.fi>
2020-04-28 15:10:53 +0300

    fixing loading game info pages

--- application/views/pages/aboot.php
+++ application/views/pages/aboot.php
@@ -8,7 +8,7 @@ if(fopen(base_url('assests/textf/'.$loadinfo.'.php'), 'r')){
     echo fread($file, filesize('assests/textf/'.$loadinfo.'.php'));
     fclose($file);
 }
-/*else{
+else{
 
 if(strpos($loadinfo, '_')){
 $loadinfo = str_replace('_', ' ', $loadinfo);
@@ -18,7 +18,7 @@ $loadinfo = str_replace('  ', ': ', $loadinfo);
 }
     $pageifnonefound = $loadinfo." is a game about things where you do stuff. <br>It was released at some point, near the release of another game. <br>Core features include nothing and everything.";
     echo $pageifnonefound;
-}*/
+}
 ?>
 
 </div>
